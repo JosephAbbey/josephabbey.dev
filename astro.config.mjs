563deee620ec1d80754404de17497a2112bdf5bf
Joseph Abbey <me@josephabbey.dev>
2025-09-15 13:20:16 +0100

    Bootstrap Shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
