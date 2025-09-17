02e1ebbb3e7e08e2f38d26dacb518afd87b44d00
Joseph Abbey <me@josephabbey.dev>
2025-09-17 15:51:38 +0100

    Fix import errors

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: "Joseph Abbey",
+    description: "Joseph Abbey's blog",
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
